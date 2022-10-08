e543164ed506988462af9fde159554efb0bdb1aa
hannes <hannesdelbeke@gmail.com>
2022-10-08 12:36:11 +0100

    cleanup

--- openmenu/marmoset.py
+++ openmenu/marmoset.py
@@ -5,10 +5,12 @@ so we create windows with buttons instead, submenus create new windows
 
 import mset
 
-w = mset.UIWindow()
+dockable_window = None
 def setup_menu(data):
-    global w
-    _setup_menu_items(w, data.get('items'))
+    # save window in a global to prevent garbage collection
+    global dockable_window
+    dockable_window = mset.UIWindow("OpenMenu")
+    _setup_menu_items(dockable_window, data.get('items'))
 
 
 def _setup_menu_items(parent_menu, items: list):
@@ -20,6 +22,7 @@ def _setup_menu_items(parent_menu, items: list):
         print(item, label)
         command = item.get('command', None)
         if command:
+            pass
             add_to_menu(parent_menu, label, command)
         else:  # submenu
             items = item.get('items', [])
@@ -27,15 +30,19 @@ def _setup_menu_items(parent_menu, items: list):
             _setup_menu_items(sub_menu, items)
 
 
-windows = []
 def add_sub_menu(parent, label: str):
-    dockable_window = mset.UIWindow()
-    dockable_window.title = label
 
-    global windows
-    windows.append(dockable_window)
 
-    return dockable_window
+    # window = mset.UIWindow("mar.pipe v.01")
+    settings_drawer_ui = mset.UIDrawer(name=label)
+    settings_drawer = mset.UIWindow(name="", register=False)
+    settings_drawer_ui.containedControl = settings_drawer
+
+    parent.addReturn()  # work vertically since else window looks bad
+    parent.addElement(settings_drawer_ui)
+    parent.addReturn()
+
+    return settings_drawer
 
 
 
@@ -45,7 +52,6 @@ def add_to_menu(parent, label: str, command: str):
             exec(command)
         else:
             command()
-    # commands.append(cmd)
 
     button = mset.UIButton()
     button.text = label
