31ce4bb0727919d718a7d0b58ba8424bfbc41132
hannes <hannesdelbeke@gmail.com>
2022-10-09 23:22:18 +0100

    change import for openmenu

--- openmenu/__init__.py
+++ openmenu/__init__.py
@@ -1,6 +1,6 @@
-import openmenu.core
+from openmenu.core import *
 
-config_setup = openmenu.core.config_setup
-setup = openmenu.core.setup
-
-
+# main menu is the main menu bar
+# window is the window that contains the main menu bar
+# parent can be the main menu bar or a submenu, or a window depending on dcc
+# TODO standardize this
