7da729de6895a31a18f55280dd787b6635cbf27b
hannes <hannesdelbeke@gmail.com>
2022-11-15 22:46:55 +0000

    krita - class setup - tested

--- openmenu/dccs/krita.py
+++ openmenu/dccs/krita.py
@@ -1,46 +1,41 @@
 # Krita has native PyQt5 support
 import PyQt5.QtWidgets as QtWidgets
 from PyQt5.QtWidgets import QMenu, QApplication
+from openmenu.dccs._abstract import AbstractMenuMaker
 
 
-def setup_menu(data):
-    # get main menu as parent
-    for widget in QApplication.topLevelWidgets():
-        if "MainWindow" in widget.objectName():
-            main_window = widget
-            break
-    main_menu = main_window.findChild(QtWidgets.QMenuBar)
+class MenuMaker(AbstractMenuMaker):
+    @classmethod
+    def setup_menu(cls, data):
+        parent = data.get("parent_menu")
 
-    _setup_menu_items(main_menu, data.get('items'))
+        if not parent:
+            # get main menu as parent
+            for widget in QApplication.topLevelWidgets():
+                if "MainWindow" in widget.objectName():
+                    main_window = widget
+                    break
+            parent = main_window.findChild(QtWidgets.QMenuBar)
 
+        return cls._setup_menu_items(parent, data.get('items'))
 
-def _setup_menu_items(parent_menu, items: list):
-    """
-    recursively add all menu items and submenus
-    """
-    for item in items:
-        label = item.get('label')
-        print(item, label)
-        command = item.get('command', None)
-        if command:
-            add_to_menu(parent_menu, label, command)
-        else:  # submenu
-            items = item.get('items', [])
-            sub_menu = add_sub_menu(parent_menu, label)
-            _setup_menu_items(sub_menu, items)
+    @classmethod
+    def add_sub_menu(cls, parent: QMenu, label: str) -> QMenu:
+        return parent.addMenu(label)
 
+    @classmethod
+    def add_to_menu(cls, parent: QMenu, label: str, command, icon: str = None, tooltip: str = None):
+        if isinstance(command, str):
+            return parent.addAction(label, lambda: exec(command))
+        else:  # callable
+            return parent.addAction(label, lambda: command())
 
-def add_sub_menu(parent: QMenu, label: str) -> QMenu:
-    return parent.addMenu(label)
+    @classmethod
+    def add_separator(cls, parent) -> "QAction":
+        return parent.addSeparator()
 
+    @classmethod
+    def teardown_menu(cls):
+        raise NotImplementedError("not yet implemented")
 
-def add_to_menu(parent: QMenu, label: str, command):
-    if isinstance(command, str):
-        return parent.addAction(label, lambda: exec(command))
-    else:  # callable
-        return parent.addAction(label, lambda: command())
-
-
-def breakdown():
-    """remove from menu"""
-    raise NotImplementedError("not yet implemented")
+setup_menu = MenuMaker.setup_menu
