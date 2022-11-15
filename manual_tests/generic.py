d4b13963ce554553b467cb3e586a7d951e570b72
hannes <hannesdelbeke@gmail.com>
2022-11-15 23:31:43 +0000

    make test generic

--- manual_tests/generic.py
+++ manual_tests/generic.py
@@ -2,20 +2,23 @@
 sample for testing
 run in Krita from the menu: Tools/scripts/scripter
 """
-import sys
-sys.path.append(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu")
-
+from openmenu.dccs import detect_dcc
 from importlib import reload
-import openmenu
-import openmenu.dccs.krita as k
-reload(k)
 from pathlib import Path
+import openmenu
+import sys
+
+
+dcc = detect_dcc()
+reload(dcc.menu_module)
+
+sys.path.append(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu")
+config = Path(openmenu.__file__).parent.parent / "samples/config.json"
 
 # add single entry to menu
 menu1 = openmenu.add_item("menu1")
 
 # add menu tree from config
-config = Path(openmenu.__file__).parent.parent / "samples/config.json"
 menu2 = openmenu.setup_config(config)[0]
 print(menu2)
 
