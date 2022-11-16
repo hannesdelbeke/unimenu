b8fab82f93694a9cb661a22db43a2ae8d36abba7
hannes <hannesdelbeke@gmail.com>
2022-11-16 00:18:39 +0000

    test updates for substance

--- manual_tests/generic.py
+++ manual_tests/generic.py
@@ -1,31 +1,54 @@
 """
 sample for testing
+
 run in Krita from the menu: Tools/scripts/scripter
+
+in substance painter,
+open Python/Plugins folder
+place substance painter test in the folder
+click Python/Reload plugins folder
+a test entry should appear in the menu, click it to run the test
 """
+
+import sys
+sys.path.append(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu")
+
+import openmenu
 from openmenu.dccs import detect_dcc
 from importlib import reload
 from pathlib import Path
-import openmenu
-import sys
 
 
-dcc = detect_dcc()
-reload(dcc.menu_module)
+def create_test_menus():
+    dcc = detect_dcc()
+    reload(dcc.menu_module)
 
-sys.path.append(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu")
-config = Path(openmenu.__file__).parent.parent / "samples/config.json"
 
-# add single entry to menu
-menu1 = openmenu.add_item("menu1")
+    config = Path(openmenu.__file__).parent.parent / "samples/config.json"
+    # in substance painter, Path(openmenu.__file__) does not return this file's location but instead:
+    # 'C:\\Users\\hanne\\OneDrive\\Documents\\Adobe\\Adobe Substance 3D Painter\\python\\modules\\samples\\config.json'
+    config = Path(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu\samples\config.json")
 
-# add menu tree from config
-menu2 = openmenu.setup_config(config)[0]
-print(menu2)
+    # add single entry to menu
+    menu1 = openmenu.add_item("menu1")
 
-# add submenu to menu
-openmenu.add_item("menu3", parent=menu2)
+    # add menu tree from config
+    menu2 = openmenu.setup_config(config)[0]
 
-# add action to menu
-menu_entry = openmenu.add_item("print hi", command="print('hi')", parent=menu2)
-print(menu_entry)
+    # print(menu2)
+    # in susbtance painter, this throws a
+    # [Python] RuntimeError: Internal C++ object (PySide2.QtWidgets.QMenu) already deleted.
 
+    # add submenu to menu
+    sub_menu1 = openmenu.add_item("menu3", parent=menu2)
+
+    # add action to menu
+    menu_entry = openmenu.add_item("print hi", command="print('hi')", parent=menu2)
+
+    print(menu_entry)
+    # in susbtance painter, this throws a
+    # [Python] RuntimeError: Internal C++ object (PySide2.QtWidgets.QAction) already deleted.
+
+
+# if __name__ == "__main__":
+#     run_tests()
---
+++ manual_tests/substance_painter_test.py
@@ -0,0 +1,7 @@
+import generic
+
+def start_plugin():
+    generic.create_test_menus()
+    # todo currently runs into c++ issues. as soon as we parent the menu to the buildin UI (c++)
+    # the python var gets garbage collected and any further parenting wont work.
+    # this does work correctly when loaded from a config though
