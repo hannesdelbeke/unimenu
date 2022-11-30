cde6aea80f6395c5d3cf51ed53a6a11b1ec2606c
hannes <hannesdelbeke@gmail.com>
2022-11-30 03:40:18 -0800

    classify maya module - tested

--- openmenu/dccs/maya.py
+++ openmenu/dccs/maya.py
@@ -1,53 +1,40 @@
 import pymel.core as pm  # todo replace with cmds because it's faster
 
 
-def setup_menu(data):
-    menu = create_root_menu("OpenMenu")
-    _setup_menu_items(menu, data.get("items"))
+from openmenu.dccs._abstract import AbstractMenuMaker
 
 
-def _setup_menu_items(parent_menu, items: list):
-    """
-    recursively add all menu items and submenus
-    """
-    print("items, ", items)
-    for item in items:
+class MenuMaker(AbstractMenuMaker):
 
-        # check if item is a dict, the divider is a str
-        if not isinstance(item, dict):
-            continue
+    @classmethod
+    def setup_menu(cls, data):
+        menu = cls.create_root_menu("OpenMenu")
+        cls._setup_menu_items(menu, data.get("items"))
 
-        label = item.get("label")
-        command = item.get("command", None)
-        if command:
-            add_to_menu(parent_menu, label, command)
-        else:  # submenu
-            items = item.get("items", [])
-            sub_menu = add_sub_menu(parent_menu, label)
-            _setup_menu_items(sub_menu, items)
+    @classmethod
+    def create_root_menu(cls, label, window_name="gMainWindow"):
+        """
+        Create a root menu in Maya
+        label: str, the label of the menu
+        window_name: str, the name of the window to attach the menu to
+        """
+        maya_window = pm.language.melGlobals[window_name]
+        return pm.menu(label, parent=maya_window)
 
+    @classmethod
+    def add_sub_menu(cls, parent, label: str):
+        return pm.menuItem(subMenu=True, label=label, parent=parent)
 
-def create_root_menu(label, window_name="gMainWindow"):
-    """
-    Create a root menu in Maya
-    label: str, the label of the menu
-    window_name: str, the name of the window to attach the menu to
-    """
-    maya_window = pm.language.melGlobals[window_name]
-    return pm.menu(label, parent=maya_window)
+    # https://help.autodesk.com/cloudhelp/2018/JPN/Maya-Tech-Docs/PyMel/generated/functions/pymel.core.windows/pymel.core.windows.menuItem.html
+    @classmethod
+    def add_to_menu(cls, parent, label: str, command: str, icon: str = "", tooltip: str = ""):
+        # menuItem doesn't support tooltip.
+        # could use qt instead http://discourse.techart.online/t/is-there-a-way-to-get-tooltips-for-maya-menitem/15385
+        return pm.menuItem(label=label, command=command, parent=parent, image=icon)
 
+    @classmethod
+    def teardown_menu(cls):
+        """remove from menu"""
+        raise NotImplementedError("not yet implemented")
 
-def add_sub_menu(parent, label: str):
-    return pm.menuItem(subMenu=True, label=label, parent=parent)
-
-
-# https://help.autodesk.com/cloudhelp/2018/JPN/Maya-Tech-Docs/PyMel/generated/functions/pymel.core.windows/pymel.core.windows.menuItem.html
-def add_to_menu(parent, label: str, command: str, icon: str = "", tooltip: str = ""):
-    # menuItem doesn't support tooltip.
-    # could use qt instead http://discourse.techart.online/t/is-there-a-way-to-get-tooltips-for-maya-menitem/15385
-    return pm.menuItem(label=label, command=command, parent=parent, image=icon)
-
-
-def breakdown():
-    """remove from menu"""
-    raise NotImplementedError("not yet implemented")
+setup_menu = MenuMaker.setup_menu
