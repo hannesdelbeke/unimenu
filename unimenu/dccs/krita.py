0eec9d51b684460c5ce4568ab7917a49b2578086
hannes <hannesdelbeke@gmail.com>
2023-01-20 22:58:07 +0000

    refactor krita - untested

--- unimenu/dccs/krita.py
+++ unimenu/dccs/krita.py
@@ -7,17 +7,17 @@ from unimenu.dccs._abstract_qt import AbstractMenuMaker
 class MenuMaker(AbstractMenuMaker):
     @classmethod
     def setup_menu(cls, data):
-        parent = data.get("parent_menu")
-
-        if not parent:
-            # get main menu as parent
-            for widget in QApplication.topLevelWidgets():
-                if "MainWindow" in widget.objectName():
-                    main_window = widget
-                    break
-            parent = main_window.findChild(QtWidgets.QMenuBar)
-
+        parent = data.get("parent_menu") or krita_main_menu_bar()
         return cls._setup_menu_items(parent, data.get("items"))
 
 
 setup_menu = MenuMaker.setup_menu
+
+
+def krita_main_menu_bar() -> QtWidgets.QMenuBar:
+    """get the main menu widget by name from Krita"""
+    for widget in QApplication.topLevelWidgets():
+        if "MainWindow" in widget.objectName():
+            main_window = widget
+            break
+    return main_window.findChild(QtWidgets.QMenuBar)
