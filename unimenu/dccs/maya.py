cc12ffb27e1e126771d5093d9e987635ae41827e
hannes <hannesdelbeke@gmail.com>
2023-02-04 16:52:56 +0000

    refactor maya, remove menumaker

--- unimenu/dccs/maya.py
+++ unimenu/dccs/maya.py
@@ -1,60 +1,7 @@
 import pymel.core as pm  # todo replace with cmds because it's faster
-from unimenu.dccs._abstract import AbstractMenuMaker, MenuNodeAbstract
+from unimenu.dccs._abstract import MenuNodeAbstract
 import maya.mel
 import maya.cmds
-
-
-class MenuMaker(AbstractMenuMaker):
-    @classmethod
-    def setup_menu(cls, data) -> pm.menu:
-        label = data.get("label")  # root menu name
-        parent = data.get("parent")  # todo parent
-
-        # todo parent logic currently is re implemented in every dcc module
-        #  can we move it to the abstract class?
-        # if we provide a parent in the config, we might want to parent to a submenu
-        if parent:
-            menu = find_menu(parent)
-        else:
-            menu = cls.create_root_menu(label)
-
-        cls._setup_menu_items(menu, data.get("items"))
-        return menu
-
-    @classmethod
-    def create_root_menu(cls, label, window_name=None):
-        """
-        Create a root menu in Maya
-        label: str, the label of the menu
-        window_name: str, the name of the window to attach the menu to
-        """
-        window_name = window_name or "gMainWindow"  # default value
-
-        maya_window = pm.language.melGlobals[window_name]
-        return pm.menu(label, parent=maya_window, tearOff=True)  # todo garantuee uniqye name
-
-    @classmethod
-    def add_sub_menu(cls, parent, label: str):
-        return pm.menuItem(label, subMenu=True, label=label, parent=parent, tearOff=True)
-
-    @classmethod
-    def add_to_menu(cls, parent, label: str, command: str, icon: str = None, tooltip: str = None):
-        icon = icon or ""
-        tooltip = tooltip or ""
-
-        return pm.menuItem(label=label, command=command, parent=parent, image=icon)
-        # todo menuItem doesn't support tooltip.
-        #  could use qt instead http://discourse.techart.online/t/is-there-a-way-to-get-tooltips-for-maya-menitem/15385
-
-    @classmethod
-    def teardown_menu(cls, name: str):
-        """remove from menu"""
-        # see https://stackoverflow.com/questions/44142119/remove-menu-item-in-maya-using-python
-        pm.deleteUI(name, menu=True)
-
-    @classmethod
-    def add_separator(cls, parent, label: str = None):
-        return pm.menuItem(divider=True, parent=parent, dividerLabel=label)
 
 
 def find_menu(name):
@@ -69,13 +16,20 @@ def find_menu(name):
         # TODO get their children recursively
 
 
+def create_root_menu(label, window_name=None) -> pm.menu:
+    """
+    Create a root menu in Maya
+    label: str, the label of the menu
+    window_name: str, the name of the window to attach the menu to
+    """
+    window_name = window_name or "gMainWindow"  # default value
 
-setup_menu = MenuMaker.setup_menu
-teardown_menu = MenuMaker.teardown_menu
+    maya_window = pm.language.melGlobals[window_name]
+    return pm.menu(label, parent=maya_window, tearOff=True)  # todo garantuee unique name
 
 
 class MenuNodeMaya(MenuNodeAbstract):
-
+    counter = 0
     @property
     def _default_root_parent(self):
 
@@ -85,16 +39,37 @@ class MenuNodeMaya(MenuNodeAbstract):
         if self.parent_path:
             menu = find_menu(self.parent_path)
         else:
-            menu = MenuMaker.create_root_menu(self.label)
+            menu = create_root_menu(self.label)
+        return menu
 
     def _setup_sub_menu(self, parent_app_node=None):
-        MenuMaker.add_sub_menu(parent=parent_app_node, label=self.label)
+        # todo handle uniqyue name, atm label can clash with other menu items
+
+        self.counter += 1
+        self.name = f"{self.label}_{self.counter}"
+
+        return pm.menuItem(self.name, subMenu=True, label=self.label, parent=parent_app_node, tearOff=True)
+
 
     def _setup_menu_item(self, parent_app_node=None):
-        MenuMaker.add_to_menu(parent=parent_app_node, label=self.label, command=self.command, icon=self.icon, tooltip=self.tooltip)
+        icon = self.icon or ""
+        tooltip = self.tooltip or ""
+
+        self.counter += 1
+        self.name = f"{self.label}_{self.counter}"
+
+        return pm.menuItem(self.name, label=self.label, command=self.command, parent=parent_app_node, image=icon)
+        # todo menuItem doesn't support tooltip.
+        #  could use qt instead http://discourse.techart.online/t/is-there-a-way-to-get-tooltips-for-maya-menitem/15385
+
 
     def _setup_separator(self, parent_app_node=None):
-        MenuMaker.add_separator(parent=parent_app_node, label=self.label)
+        self.counter += 1
+        self.name = f"{self.label}_{self.counter}"
+
+        return pm.menuItem(self.name, divider=True, parent=parent_app_node, dividerLabel=self.label)
 
     def teardown(self):
-        MenuMaker.teardown_menu(self.label)
+        # see https://stackoverflow.com/questions/44142119/remove-menu-item-in-maya-using-python
+        # todo can we remove self.name? bit hacky
+        pm.deleteUI(self.name, menu=True)
