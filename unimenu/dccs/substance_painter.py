175b5e589fc6ee8a9ab755dcfa9b5b3b28a52c50
hannes <hannesdelbeke@gmail.com>
2023-02-04 09:06:41 +0000

    make menunode substance - untested

--- unimenu/dccs/substance_painter.py
+++ unimenu/dccs/substance_painter.py
@@ -1,20 +1,10 @@
 # substance painter has native PySide2 support
 import PySide2.QtWidgets as QtWidgets
 from PySide2.QtWidgets import QMenu, QApplication
-from unimenu.dccs.qt import QtMenuMaker
+from unimenu.dccs.qt import QtMenuMaker, MenuNodeQt
 
 # it seems substance's native UI qwidgets aren't accessible
 # substance_painter.ui.ApplicationMenu is not a QMenu
-
-
-class MenuMaker(QtMenuMaker):
-    @classmethod
-    def setup_menu(cls, data):
-        parent = data.get("parent") or main_menu_bar()
-        return cls._setup_menu_items(parent, data.get("items"))
-
-
-setup_menu = MenuMaker.setup_menu
 
 
 # todo can we move this to qt class. inherit get_main_menu_bar for all DCCs
@@ -27,3 +17,10 @@ def main_menu_bar() -> QtWidgets.QMenuBar:
             if widget.objectName() == "edit":
                 main_menu = widget.parent()
     return main_menu
+
+
+class MenuNodeSubstance(MenuNodeQt):
+    @property
+    def _default_root_parent(self):
+        """get the default parent for the root node, optional method"""
+        return main_menu_bar()
