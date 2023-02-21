4fb8e7ec81f32f195950d17bafa977843ba82884
hannes <hannesdelbeke@gmail.com>
2023-02-21 10:43:39 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unimenu',
+    version='0.2.1',
+    packages=['unimenu', 'unimenu.dccs'],
+    url='https://github.com/hannesdelbeke/unimenu',
+    license='',
+    author='h',
+    author_email='',
+    description='easy consistent menus across dcc apps'
+)
