56d4c2510a2e4b924f44ac3d200730a72add4075
hannes <hannesdelbeke@gmail.com>
2023-03-02 14:17:34 +0000

    update test to add icon and labeled separator and submenu and tooltip

--- samples/any_dcc_test.py
+++ samples/any_dcc_test.py
@@ -5,6 +5,12 @@ piece of test-code for copy/pasting, that should work in every app
 import sys
 sys.path.append(r"C:\Users\hanne\OneDrive\Documents\repos\openmenu")
 import unimenu
+import unimenu.apps.qt as q
+import unimenu.apps._abstract as a
+from importlib import reload
+reload(q)
+reload(a)
+reload(unimenu)
 
 
 config = {"label": "UniMenu","items": [{"label": "Item 1","command": "print('Item 1')"},{"label": "Item 2","command": "print('Item 2')"}]}
@@ -21,6 +27,44 @@ config = {
             "command": "print('Item 2')"
         }
     ]
+}
+
+
+config = {
+    'label': 'Tools',
+    'items':
+        [
+            {
+                'command': 'print("hello 1")',
+                'label': 'tool1',
+                "icon": ":/qt-project.org/styles/commonstyle/images/up-32.png",
+                "tooltip": "tooltip"
+            },
+            {
+                'label': 'label',
+                'separator': True,
+                "icon": ":/qt-project.org/styles/commonstyle/images/up-32.png",
+                "tooltip": "tooltip separator"
+            },
+            {
+                'command': 'print("hello 2")',
+                'label': 'tool2'
+            },
+            {
+                'label': 'Tools',
+                'items':
+                    [
+                        {
+                            'command': 'print("hello 1")',
+                            'label': 'tool1'
+                        },
+                        {
+                            'command': 'print("hello 2")',
+                            'label': 'tool2'
+                        }
+                    ]
+            }
+        ]
 }
 
 app_menu_node = unimenu.setup(config)
