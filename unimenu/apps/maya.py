edca7f164ac39510e2082eb72f32f8d03da9d5f7
hannes <hannesdelbeke@gmail.com>
2023-03-16 16:31:17 -0700

    remove counter(unique name) and pymel in native maya, tested ☑

--- unimenu/apps/maya.py
+++ unimenu/apps/maya.py
@@ -1,20 +1,9 @@
 """
 native maya menu support. not needed anymore, use the qt menu instead
 """
-
-import pymel.core as pm  # todo replace with cmds because it's faster
 from unimenu.apps._abstract import MenuNodeAbstract
 import maya.mel
-import maya.cmds
-
-
-counter = 0
-
-
-def get_counter():
-    global counter
-    counter += 1
-    return counter
+import maya.cmds  # cmds is faster than pymel
 
 
 def find_menu(name):
@@ -28,25 +17,27 @@ def find_menu(name):
         # TODO get their children recursively
 
 
-def create_root_menu(label, window_name=None, kwargs=None) -> pm.menu:
+def create_root_menu(label, window_name=None, kwargs=None) -> maya.cmds.menu:
     """
     Create a root menu in Maya
     label: str, the label of the menu
     window_name: str, the name of the window to attach the menu to
     """
-    window_name = window_name or "gMainWindow"  # default value
-    maya_window = pm.language.melGlobals[window_name]
+    # todo add support for menus in other windows, e.g. the Script Editor
+    # # lsUI() might be able to replace pymel
+    # import pymel.core as pm
+    # window_name = window_name or "gMainWindow"  # default value
+    # maya_window = pm.language.melGlobals[window_name]  # returns "MayaWindow", type str
+    maya_window = "MayaWindow"  # hardcode main window for now
 
     # support adding custom kwargs from the config
     kwargs = kwargs or {}
     kwargs.setdefault("parent", maya_window)
     kwargs.setdefault("tearOff", True)
 
-    # we require a predictable name to parent menus to from other configs, so no counter
-    # name = f"{label}_{get_counter()}"
     name = label.replace("_", " ")  # maya menu names can't have underscores
 
-    return pm.menu(name, **kwargs)
+    return maya.cmds.menu(name, **kwargs)
 
 
 class MenuNodeMaya(MenuNodeAbstract):
@@ -75,8 +66,8 @@ class MenuNodeMaya(MenuNodeAbstract):
             kwargs.setdefault("label", self.label)
             kwargs.setdefault("parent", parent_app_node)
 
-            self.name = f"{self.label}_{get_counter()}"
-            return pm.menuItem(self.name, **kwargs)
+            self.name = self.label
+            return maya.cmds.menuItem(self.name, **kwargs)
 
     def _setup_menu_item(self, parent_app_node=None):
         icon = self.icon or ""
@@ -90,12 +81,12 @@ class MenuNodeMaya(MenuNodeAbstract):
         kwargs.setdefault("image", icon)
         kwargs.setdefault("annotation", tooltip)  # shown on hover in lower left corner
 
-        self.name = f"{self.label}_{get_counter()}"
+        self.name = self.label
 
-        return pm.menuItem(self.name, **kwargs)
+        return maya.cmds.menuItem(self.name, **kwargs)
 
     def _setup_separator(self, parent_app_node=None):
-        self.name = f"{self.label}_{get_counter()}"
+        self.name = self.label
 
         # support adding custom kwargs from the config
         kwargs = self.kwargs
@@ -103,9 +94,10 @@ class MenuNodeMaya(MenuNodeAbstract):
         kwargs.setdefault("dividerLabel", self.label)
         kwargs.setdefault("parent", parent_app_node)
 
-        return pm.menuItem(self.name, **kwargs)
+        return maya.cmds.menuItem(self.name, **kwargs)
 
     def teardown(self):
         # see https://stackoverflow.com/questions/44142119/remove-menu-item-in-maya-using-python
         # todo can we remove self.name? bit hacky
-        pm.deleteUI(self.name, menu=True)
+        maya.cmds.deleteUI(self.name, menu=True)
+
