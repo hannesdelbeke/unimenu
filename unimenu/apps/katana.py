c217a6fdbb0c865ecb46d2bf433486c886abb21a
zuokangbo <1156298563@qq.com>
2023-05-07 19:58:17 +0800

    add katana item

---
+++ unimenu/apps/katana.py
@@ -0,0 +1,32 @@
+from unimenu.apps._abstract import MenuNodeAbstract
+from UI4.App import MainWindow, MainMenu
+import logging
+
+
+class MenuNodeQt(MenuNodeAbstract):
+
+    @property
+    def _default_root_parent(self):
+        menu_bar = MainWindow.GetMainWindow().getMenuBar()
+        return menu_bar
+
+    def _setup_sub_menu(self, parent_app_node=None):
+        if parent_app_node:
+            return parent_app_node.addMenu(self.label)
+
+    def _setup_menu_item(self, parent_app_node=None):
+        if parent_app_node:
+            action = parent_app_node.addAction(self.label)
+            action.setParent(parent_app_node)
+            action.triggered.connect(self.run)
+            return action
+
+    def _setup_separator(self, parent_app_node=None):
+        action = self._setup_menu_item(parent_app_node=parent_app_node)
+        action.setSeparator(True)
+        return action
+
+    def teardown(self):
+        """remove from menu"""
+        self.app_node.deleteLater()
+
