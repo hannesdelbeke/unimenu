62fd35aa851d98f08be4943b81173a5c7d661bea
haydeJL <hayden230196@yahoo.co.uk>
2023-07-13 20:25:38 +0100

    updating test

--- samples/qt_system_tray.py
+++ samples/qt_system_tray.py
@@ -1,45 +1,72 @@
 import sys
 
-from Qt import QtWidgets, QtGui
+from Qt import QtWidgets, QtGui, QtCore
 import unimenu
 
+config = {
+    'label': 'Tools',
+    'items':
+        [
+            {
+                'command': 'print("hello 1")',
+                'label': 'tool1',
+                "icon": ":/qt-project.org/styles/commonstyle/images/up-32.png",
+                "tooltip": "tooltip"
+            },
+            {
+                'label': 'label',
+                'separator': True,
+                "icon": ":/qt-project.org/styles/commonstyle/images/up-32.png",
+                "tooltip": "tooltip separator"
+            },
+            {
+                'command': 'print("hello 2")',
+                'label': 'tool2'
+            },
+            {
+                'label': 'Tools',
+                'items':
+                    [
+                        {
+                            'command': 'print("hello 1")',
+                            'label': 'tool1'
+                        },
+                        {
+                            'command': 'print("hello 2")',
+                            'label': 'tool2'
+                        }
+                    ]
+            }
+        ]
 
-class SystemTrayTest(QtWidgets.QMainWindow):
+}
+
+
+class SystemTrayTest(QtWidgets.QSystemTrayIcon):
     def __init__(self):
         super(SystemTrayTest, self).__init__(parent=None)
-        self.setObjectName('mainWindow')
 
         pixmap = QtGui.QPixmap(32, 32)
         pixmap.fill(QtGui.QColor(255, 0, 0))
-        self.tray = QtWidgets.QSystemTrayIcon(None)
-        self.tray.setIcon(QtGui.QIcon(pixmap))
-        self.tray.show()
+        self.setIcon(QtGui.QIcon(pixmap))
+        self.show()
 
-        self.main_widget_menu = QtWidgets.QMenu(self)
-        self.tray.setContextMenu(self.main_widget_menu)
-        self.tray.activated.connect(self.show_menu_on_trigger)
+        self.main_widget_menu = QtWidgets.QMenu()
+        self.main_widget_menu.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.setContextMenu(self.main_widget_menu)
+        self.activated.connect(self.show_menu_on_trigger)
 
-        menu = unimenu.Node(label="test", use_menu_bar=False)
-        menu2 = unimenu.Node(label="test1", use_menu_bar=False)
-        item1 = unimenu.Node(label="bye", command=lambda: print("Hello"))
-        item = unimenu.Node(label="hi")
-        sub_item = unimenu.Node(label="test", command=lambda: print("Hello"))
-        item.items.append(sub_item)
-        menu.items.append(item)
-        menu2.items.append(item1)
-        menu.setup()
-        menu2.setup()
-
-        exit_action = unimenu.Node(label="Exit", command=lambda: sys.exit(), use_menu_bar=False)
-        exit_action.setup()
+        self.app_menu_node = unimenu.setup(config, parent_app_node=self.main_widget_menu)
+        self.app_exit = unimenu.setup({"label": "Exit", "command": lambda: sys.exit()},
+                                      parent_app_node=self.main_widget_menu)
 
     def show_menu_on_trigger(self, reason):
         if reason == QtWidgets.QSystemTrayIcon.Trigger:
-            self.tray.contextMenu().popup(QtGui.QCursor.pos())
+            self.contextMenu().popup(QtGui.QCursor.pos())
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     tray = SystemTrayTest()
-    tray.tray.show()
+    tray.show()
     app.exec_()
