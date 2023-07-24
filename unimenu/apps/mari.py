f5270c45327d8048832b054beba05cce91dfb101
hannes <hannesdelbeke@gmail.com>
2023-07-24 12:25:19 +0100

    add , **self.kwargs to mari

--- unimenu/apps/mari.py
+++ unimenu/apps/mari.py
@@ -16,13 +16,14 @@ class MenuNodeMari(MenuNodeAbstract):
             return f"{parent_app_node}/{self.label}"
 
     def _setup_menu_item(self, parent_app_node=None):
+        # todo unsure if **self.kwargs should be passed to mari.actions.create or mari.menus.addAction
         if parent_app_node:
             action = mari.actions.create(f"{parent_app_node}/{self.label}", self.command or "")
             mari.menus.addAction(action, parent_app_node)
 
     def _setup_separator(self, parent_app_node=None):
         if parent_app_node:
-            mari.menus.addSeparator(parent_app_node)
+            mari.menus.addSeparator(parent_app_node, **self.kwargs)
 
     def teardown(self):
         """remove from menu"""
