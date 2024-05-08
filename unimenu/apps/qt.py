d33741f975fee828abc399272ec8215a41a4e096
hannes <hannesdelbeke@gmail.com>
2024-05-08 23:31:55 +0100

    fix maya bug & refactor code
    breakup in methods

--- unimenu/apps/qt.py
+++ unimenu/apps/qt.py
@@ -19,8 +19,41 @@ with contextlib.suppress(ImportError):
 
 menu_bar = None
 main_window = None
-class MenuNodeQt(MenuNodeAbstract):
 
+
+def find_main_window():
+    # ideally there is only 1 main window, but there could be more
+    # e.g. Maya has a main window for arnold
+    widgets = [w for w in QtWidgets.QApplication.topLevelWidgets() if type(w) == QtWidgets.QMainWindow]
+    if len(widgets) == 1:
+        return widgets[0]
+    else:
+        logging.warning(f"found {len(widgets)} main windows,  can't auto select main window")
+
+    # a lot of apps have a main window named "mainWindow"
+    if not main_window:
+        for widget in QtWidgets.QApplication.topLevelWidgets():
+            if widget.objectName() in ("mainWindow", "MayaWindow"):  # MayaWindow hardcode hack
+                # print("FOUND MAIN WINDOW UNIMENU", main_window)
+                return widget
+
+
+def find_main_menu_bar(main_window, parent_path):
+    """
+    get the main menu bar widget from a main window,
+    or the sub menu widget if a sub path is provided
+    """
+    menu_bar = main_window.findChild(QtWidgets.QMenuBar)
+    if parent_path:
+        parent_menu = menu_bar.findChild(QtWidgets.QMenu, parent_path)
+        if not parent_menu:
+            logging.warning("Parent menu not found, using main menu bar")
+        else:
+            menu_bar = parent_menu
+    return menu_bar
+
+
+class MenuNodeQt(MenuNodeAbstract):
     @property
     def _default_root_parent(self):
         """
@@ -28,32 +61,12 @@ class MenuNodeQt(MenuNodeAbstract):
         it finds the first QMainWindow in the app, and gets its menu bar
         if this is not the desired behaviour, override this method or set the parent of the root node before setup
         """
-
         # store in global, to avoid garbage collection of python pointer to c++ qt obj
-        global menu_bar
         global main_window
+        global menu_bar
 
-        for widget in QtWidgets.QApplication.topLevelWidgets():
-            if type(widget) == QtWidgets.QMainWindow:
-                main_window = widget
-                break
-
-        if not main_window:
-            for widget in QtWidgets.QApplication.topLevelWidgets():
-                if widget.objectName() == "mainWindow":
-                    print("FOUND MAIN WINDOW UNIMENU", main_window)
-                    main_window = widget
-                    break
-
-        menu_bar = main_window.findChild(QtWidgets.QMenuBar)
-
-        if self.parent_path:
-            parent_menu = menu_bar.findChild(QtWidgets.QMenu, self.parent_path)
-            if not parent_menu:
-                logging.warning("Parent menu not found, using main menu bar")
-            else:
-                menu_bar = parent_menu
-
+        main_window = find_main_window()
+        menu_bar = find_main_menu_bar(main_window, self.parent_path)
         return menu_bar
 
     def _setup_sub_menu(self, parent_app_node=None) -> QtWidgets.QMenu:
